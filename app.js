f88b5671663250228264c35ffa3381198a7c2937
Nehal <mdnehaladil@gmail.com>
2023-06-21 09:26:57 +0530

    fetch Meal List & dets from api

--- app.js
+++ app.js
@@ -1,1 +1,102 @@
-console.log("Meal App");
+// console.log("Meal App");
+
+// Fetch Meals from api
+async function fetchMealsFromApi(url, value) {
+  const response = await fetch(`${url + value}`);
+  const meals = await response.json();
+  return meals;
+}
+
+//All Meals from api
+function showMealList() {
+  let inputValue = document.getElementById("my-search").value;
+  let arr = JSON.parse(localStorage.getItem("favouritesList"));
+  let url = "https://www.themealdb.com/api/json/v1/1/search.php?s=";
+  let html = "";
+  let meals = fetchMealsFromApi(url, inputValue);
+  meals.then((data) => {
+    if (data.meals) {
+      data.meals.forEach((element) => {
+        let isFav = false;
+        for (let index = 0; index < arr.length; index++) {
+          if (arr[index] == element.idMeal) {
+            isFav = true;
+          }
+        }
+        if (isFav) {
+          html += `
+                <div id="card" class="card mb-3" style="width: 20rem;">
+                    <img src="${element.strMealThumb}" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">${element.strMeal}</h5>
+                        <div class="d-flex justify-content-between mt-5">
+                            <button type="button" class="btn btn-outline-light" onclick="showMealDetails(${element.idMeal})">More Details</button>
+                            <button id="main${element.idMeal}" class="btn btn-outline-light active" onclick="addRemoveToFavList(${element.idMeal})" style="border-radius:50%"><i class="fa-solid fa-heart"></i></button>
+                        </div>
+                    </div>
+                </div>
+                `;
+        } else {
+          html += `
+                <div id="card" class="card mb-3" style="width: 20rem;">
+                    <img src="${element.strMealThumb}" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">${element.strMeal}</h5>
+                        <div class="d-flex justify-content-between mt-5">
+                            <button type="button" class="btn btn-outline-light" onclick="showMealDetails(${element.idMeal})">More Details</button>
+                            <button id="main${element.idMeal}" class="btn btn-outline-light" onclick="addRemoveToFavList(${element.idMeal})" style="border-radius:50%"><i class="fa-solid fa-heart"></i></button>
+                        </div>
+                    </div>
+                </div>
+                `;
+        }
+      });
+    } else {
+      html += `
+            <div class="page-wrap d-flex flex-row align-items-center">
+                <div class="container">
+                    <div class="row justify-content-center">
+                        <div class="col-md-12 text-center">
+                            <span class="display-1 d-block">404</span>
+                            <div class="mb-4 lead">
+                                The meal you are looking for was not found.
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            `;
+    }
+    document.getElementById("main").innerHTML = html;
+  });
+}
+
+//Meal Details
+async function showMealDetails(id) {
+  let url = "https://www.themealdb.com/api/json/v1/1/lookup.php?i=";
+  let html = "";
+  await fetchMealsFromApi(url, id).then((data) => {
+    html += `
+          <div id="meal-details" class="mb-5">
+            <div id="meal-header" class="d-flex justify-content-around flex-wrap">
+              <div id="meal-thumbail">
+                <img class="mb-2" src="${data.meals[0].strMealThumb}" alt="" srcset="">
+              </div>
+              <div id="details">
+                <h3>${data.meals[0].strMeal}</h3>
+                <h6>Category : ${data.meals[0].strCategory}</h6>
+                <h6>Area : ${data.meals[0].strArea}</h6>
+              </div>
+            </div>
+            <div id="meal-instruction" class="mt-3">
+              <h5 class="text-center">Instruction :</h5>
+              <p>${data.meals[0].strInstructions}</p>
+            </div>
+            <div class="text-center">
+              <a href="${data.meals[0].strYoutube}" target="_blank" class="btn btn-outline-light mt-3">Watch Video</a>
+            </div>
+          </div>
+        `;
+  });
+  document.getElementById("main").innerHTML = html;
+}
